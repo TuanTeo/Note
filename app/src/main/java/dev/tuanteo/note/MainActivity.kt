22e827e52bf2723f0a18b092e54fe7b9d9874483
tuantqd <tuantqd@bkav.com>
2022-04-28 23:27:35 +0700

    Thêm Button Back cho NavController

--- app/src/main/java/dev/tuanteo/note/MainActivity.kt
+++ app/src/main/java/dev/tuanteo/note/MainActivity.kt
@@ -3,6 +3,9 @@ package dev.tuanteo.note
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import dagger.hilt.android.AndroidEntryPoint
 import dev.tuanteo.note.databinding.ActivityMainBinding
 
@@ -10,7 +13,17 @@ import dev.tuanteo.note.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
         val binding : ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        /*TuanTeo: Thêm Back Button trên AppBar cho NavController */
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.note_nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    /*TuanTeo: Sự kiện Back Button trên AppBar cho NavController */
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.note_nav_host_fragment)
+        return navController.navigateUp()
     }
 }
